bbd6962c4a4f9d86370e3e35fb2627a6bce5088e
Miguel Barajas <migbaraj@cisco.com>
2021-03-25 17:07:52 -0600

    Arreglo de variables

--- vmware.tf
+++ vmware.tf
@@ -1,21 +1,3 @@
-terraform {
-  required_providers {
-    vsphere = {
-      source = "hashicorp/vsphere"
-      version = "1.24.3"
-    }
-  }
-}
-
-provider "vsphere" {
-  user           = var.vsphere_user
-  password       = var.vsphere_password
-  vsphere_server = var.vsphere_server
-
-  # if you have a self-signed cert
-  allow_unverified_ssl = true
-}
-
 
 data "vsphere_datacenter" "dc" {
   name = var.vsphere_datacenter
